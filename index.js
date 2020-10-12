153d3ea84254bbe014f8800ab2a840e08cb191c9
Dinhtuan1999 <nguyendinhtuan112@gmail.com>
2020-10-11 20:26:04 -0700

    hoan thanh bai tap

---
+++ index.js
@@ -0,0 +1,9 @@
+function yesClick(){
+    alert("yes.i love you")
+}
+function noHover(){
+    var x= Math.round(Math.random() * window.innerWidth);
+    var y= Math.round(Math.random() * window.innerHeight);
+    document.getElementById('btnNo').style.left = x +'px';
+    document.getElementById('btnNo').style.top = y +'px';
+}
